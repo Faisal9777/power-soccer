565a7f76247831077e661372b4b3fecd3a7d55f2
Fardin Eajdani <eajdani@shernet.sheridancollege.ca>
2025-11-02 06:47:13 +0600

    added time, goal scores, and reset positions

--- ball.gd
+++ ball.gd
@@ -1,26 +1,21 @@
 extends RigidBody3D
+class_name Ball
 #
 #func _ready() -> void:
 	#contact_monitor = true
 	#max_contacts_reported = 16
 	#print("[Ball] ready; contact monitor on.")
 #
-#func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	#if linear_velocity.x != 0:
-		#print("[Ball] _integrate_forces tick")  # prove this runs
-		#var n: int = state.get_contact_count()
-		#for i in range(n):
-			#var other_rid: RID = state.get_contact_collider(i)
-			#if not other_rid.is_valid():
-				#continue
-			#var inst_id: int = PhysicsServer3D.body_get_object_instance_id(other_rid)
-			#if inst_id == 0:
-				#continue
-			#var obj: Object = instance_from_id(inst_id)
-			#if obj is CollisionObject3D:
-				#var co := obj as CollisionObject3D
-				#var pos: Vector3 = state.get_contact_collider_position(i)
-				#var nrm: Vector3 = state.get_contact_local_normal(i)
-				#var v_at: Vector3 = state.get_contact_collider_velocity_at_position(i)
-				#print("Contact with:", co.name, " class=", co.get_class(),
-					  #" at:", pos, " normal:", nrm, " collider_v_at_pos:", v_at)
+var _last_player_hits: Array[int] = [-1, -1]
+func apply_hit(J : Vector3, position : Vector3, player_id : int) -> void:
+	if _last_player_hits.size() > 1:
+		var last_player_id = _last_player_hits[0]
+		_last_player_hits[1] = last_player_id
+
+	_last_player_hits[0] = player_id
+	apply_impulse(J,  position)
+
+func get_player(last_hit_index : int) -> int:
+	if _last_player_hits.size() < 1 or _last_player_hits.size() < 2 and last_hit_index == 1:
+		return -1
+	return _last_player_hits[last_hit_index]
