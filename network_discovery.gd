24b11dc75abcc998becc5d67ccd0d7c429d6d94e
Fardin Eajdani <eajdani@shernet.sheridancollege.ca>
2026-03-27 14:20:43 +0600

    started server list feature

---
+++ network_discovery.gd
@@ -0,0 +1,31 @@
+extends Node
+
+# NetworkDiscovery.gd
+signal server_found(server_info)
+
+var listen_udp := PacketPeerUDP.new()
+
+func start_discovery():
+	listen_udp.bind(NetConfig.DISCOVERY_PORT)
+	listen_udp.set_broadcast_enabled(true)
+
+func _process(delta):
+	# listen
+	while listen_udp.get_available_packet_count() > 0:
+		var packet = listen_udp.get_packet()
+		var ip = listen_udp.get_packet_ip()
+
+
+		var data = JSON.parse_string(packet.get_string_from_utf8())
+	   
+		if data.error != OK:
+			print("Failed to parse JSON from broadcast:", data.error_string)
+			continue
+	   
+		if data:
+			data["ip"] = ip
+			emit_signal("server_found", data)
+
+
+func stop_discovery():
+	listen_udp.close()
---
+++ scripts/multiplayer/server_info.gd
@@ -0,0 +1,9 @@
+class_name ServerInfo
+
+var name: String
+var players: int
+var max_players: int
+var ip: String
+var port: int
+var ping: int = 0
+var is_lan: bool = true
